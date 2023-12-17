16a2c8d5e5a3bb58d8426f36e076152d0355c1b2
Mariana Choratto <146736051+marianachoratto@users.noreply.github.com>
2023-12-17 11:56:55 -0300

    commit final

--- Jogo 2- Detona Ralph/src/scripts/engine.js
+++ Jogo 2- Detona Ralph/src/scripts/engine.js
@@ -10,10 +10,34 @@ const state = {
         gameVelocity: 1000,
         hitPosition: 0,
         result: 0,
+        currentTime: 60,
     },
+    actions: {// note! uma variável pode fazer uma ação
+        //poderia estar dentro da função countDown
+        //não foi chamado, mas funciona, pois está dentro da função principal
+        countDownTimerId: setInterval(countDown, 1000),
+    }
 };
 
-//função aleatória de quadrados + adicionar inimigo
+//Função do timer 
+function countDown(){
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if(state.values.currentTime <= 0){
+        clearInterval(state.actions.countDownTimerId)
+       alert(`Game Over! O seu resultado foi ${state.values.result}`) 
+    }
+}
+
+function playSound(){
+    let audio = new Audio("./src/audios/hit.m4a")
+    //diminuir som 
+    audio.volume = 0.1;
+    audio.play();
+}
+
+//Escolher um quadrado aleatório + adicionar inimigo
 function randomSquare(){
     //1º: remover inimigo
     state.view.squares.forEach((square)=>{
@@ -22,9 +46,8 @@ function randomSquare(){
     //2º Sortear um número aleatório
     let randomNumber = Math.floor(Math.random()*9)
     let randomSquare = state.view.squares[randomNumber]
-    //Em que ponto isso faz uma lista? 
     
-    //3º Colocar o inimigo dentro do numero aleatório
+    //3º Colocar o inimigo dentro do numero escolhido
     randomSquare.classList.add("enemy")
 
     //4º Guardar a posição do inimigo para fazer a comparação na função ListenerHitbox
@@ -34,9 +57,10 @@ function randomSquare(){
 //Função que move o inimigo em X tempo
 function moveEnemy(){
     state.values.timerId = setInterval(randomSquare, state.values.gameVelocity)
-    //a cada X tempo eu vou chamar a função moveEnemy para ela sortear um novo número na função randomSquare
+    // a cada 1 segundo a função setInterval sorteia um novo número em randomSquare
 }
 
+//Porém, o usuário pode clicar no inimigo antes de dar 1 seg. Nesse caso, acontece outro evento.
 //Listener de quando o usuário clica no inimigo
 function addListenerHitbox(){
     state.view.squares.forEach((square)=>{
@@ -45,18 +69,20 @@ function addListenerHitbox(){
                 // se acertar o inimigo a pontuação sobe +1
                 state.values.result++
                 state.view.score.textContent = state.values.result;
-                //impedir de aumentar + de 1 ponto 
+                //para impedir o usuário de aumentar + de 1 ponto por clique a hitposition zera automaticamente 
                 state.values.hitPosition = null
+                // toda vez que acerta toca um som 
+                playSound();
             }
         } )
     })
 }
-//Eu sempre tenho que falar para o jogo onde está meu inimigo
 
-//Toda a vez que o jogo inicia: o inimigo se move, o listener inicia
+//Toda a vez que o jogo inicia: o timer começa a contar e o listener inicia
 function initialize(){
     moveEnemy();
     addListenerHitbox();
 }
 
+//chamando a função para começar 
 initialize();
--- Jogo 2- Detona Ralph/src/scripts/oi.js
+++
@@ -1,7 +0,0 @@
-let randomNumber = Math.floor(Math.random()*9)
-
-let oi = Math.random()*10
-
-console.log(oi)
-
-//O evento mousedown é ativado quando um botão de um dispositivo apontador é pressionado sobre um elemento.
